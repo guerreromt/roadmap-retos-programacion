eb4c31c646fb5abd48262cf5342c61fe23abbb04
guerreromt <guerrerotenoriojuanmiguel9@gmail.com>
2024-05-07 19:09:13 -0500

    Create guerreromt.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/guerreromt.py
@@ -0,0 +1,11 @@
+# https://www.python.org/
+
+# comentario en una linea 
+
+"""
+esto tambien es 
+un comentario en 
+varias linesa
+"""
+my_variable = "mi variable"
+my_variable = "nuevo valor de mi variable"
